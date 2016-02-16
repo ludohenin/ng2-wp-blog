510c4b7d9d099e6937ceb565ba37c5034b6d8c2e
ludohenin <ludovic.henin@yahoo.com>
2016-02-16 17:57:20 +0100

    Update to the latest seed

--- gulpfile.ts
+++ gulpfile.ts
@@ -14,13 +14,6 @@ gulp.task('build.docs', task('build.docs'));
 gulp.task('serve.docs', task('serve.docs'));
 
 // --------------
-// Postinstall.
-gulp.task('postinstall', done =>
-  runSequence('clean',
-              'npm',
-              done));
-
-// --------------
 // Build dev.
 gulp.task('build.dev', done =>
   runSequence('clean.dist',
