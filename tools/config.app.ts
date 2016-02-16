706f78379d250c68cb320d0f4adff92d71a57dd0
ludohenin <ludovic.henin@yahoo.com>
2016-02-16 22:57:46 +0100

    chore: Fix dev app config

--- tools/config.app.ts
+++ tools/config.app.ts
@@ -3,6 +3,7 @@ import {ENV} from './config';
 const APP_CONFIG_DEV = {
   WP_API_ROOT: 'https://3dots.io/wp-blog-test/wp-json',
   WP_API_NAMESPACE: '/wp/v2',
+  DISQUS_SRC: 'https://3dotsblogtest.disqus.com/embed.js',
   GA_ID: 'UA-71460906-1'
 };
 
